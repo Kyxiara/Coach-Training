4261b25c383bf9e80009126c5b76e3ebb579eeb7
Kelly Narboux <kellynarboux@hotmail.fr>
2018-09-19 22:57:41 +0200

    Add SplashScreen

---
+++ app/src/main/java/com/example/kellynarboux/coach_training/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.kellynarboux.coach_training;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends Activity {
+    private ProgressBar mProgress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show the splash screen
+        setContentView(R.layout.activity_splash_screen);
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(500);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
