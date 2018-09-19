3b44cb75d8715d8632e74c9ee7ab3b476771592c
Hugo-C <hchastel@laposte.net>
2018-09-19 10:05:51 +0200

    Added wip textToExercise method

--- app/src/main/java/com/example/kellynarboux/coach_training/Model/Exercise.java
+++ app/src/main/java/com/example/kellynarboux/coach_training/Model/Exercise.java
@@ -1,6 +1,12 @@
 package com.example.kellynarboux.coach_training.Model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class Exercise {
+
+    // Regex to match vocal exercise
+    private static final Pattern p = Pattern.compile("\\D*(\\d+)\\s*(\\S+)");
 
     private String name;
 
@@ -12,5 +18,13 @@ public abstract class Exercise {
         return name;
     }
 
-    public abstract Exercise textToExercise(String text);
+    public static Exercise textToExercise(String text) {
+        Matcher m = p.matcher(text);
+        boolean b = m.matches();
+        if (b) {
+            return new CountableExercise(m.group(2), Integer.parseInt(m.group(1)));
+        } else {
+            return null; // no match for an exercise
+        }
+    }
 }
