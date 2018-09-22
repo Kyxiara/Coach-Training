cda8e3041d97a33f82530685930d86b44bfbf4eb
Kelly Narboux <kellynarboux@hotmail.fr>
2018-09-22 19:46:16 +0200

    WIP : ProfilActivity

--- app/src/main/java/com/example/kellynarboux/coach_training/ProfilActivity.java
+++ app/src/main/java/com/example/kellynarboux/coach_training/ProfilActivity.java
@@ -1,5 +1,6 @@
 package com.example.kellynarboux.coach_training;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,16 +10,29 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Objects;
 
 public class ProfilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
     Toolbar toolbar;
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mToogle;
     NavigationView navigationView;
+
+    ImageView avatar;
+    EditText weight;
+    EditText size;
+    TextView imc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +51,31 @@ public class ProfilActivity extends AppCompatActivity implements NavigationView.
 
         navigationView = (NavigationView) findViewById(R.id.nav_viewProfil);
         navigationView.setNavigationItemSelectedListener(this);
+
+        avatar = (ImageView) findViewById(R.id.avatar);
+        ImageButton button_man = (ImageButton) findViewById(R.id.button_man);
+        button_man.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avatar.setImageResource(R.drawable.avatar_homme);
+            }
+        });
+        ImageButton button_woman = (ImageButton) findViewById(R.id.button_woman);
+        button_woman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avatar.setImageResource(R.drawable.avatar_femme);
+            }
+        });
+
+        weight = (EditText) findViewById(R.id.weight);
+        size = (EditText) findViewById(R.id.size);
+        imc = (TextView) findViewById(R.id.imc);
+    }
+
+    public float calculIMC(float weight, int size){
+        size /= 100;
+        return weight / (size * size);
     }
 
     @Override
